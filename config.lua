64ff88d116952601d222d3f6223956bcc2995f50
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-09-23 14:07:13 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Framework = "ESX" -- "ESX", "QBCore" or "none" if none everyone can open the menu
+Config.AdminPermission = true
+
+Config.maxWaterHeight = 250.0 -- How high the Water gets until it Stops
+Config.tsunamiSpeed = 0.05 -- How many Meters the Water gets up per WaterWaitingTime
+Config.tsunamiWaitTime = 600000 -- How long the Water should stay up 600000 = 10 Minutes
+Config.WaterWaitingTime = 100 -- Set to 1000 to get an better Performance (changes on how fast the water gets higher if number is lower it is faster)
