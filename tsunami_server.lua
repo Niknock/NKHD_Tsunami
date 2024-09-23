76fcbc565e3695d3672efed9316e0029649062da
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-09-23 17:23:20 +0200

    Update tsunami_server.lua

--- tsunami_server.lua
+++ tsunami_server.lua
@@ -8,6 +8,7 @@ elseif Config.Framework == "QBCore" then
 end
 
 local nkhd_tsunamiActive = false
+local nkhd_tsunamiPaused = false
 local currentWaterHeight = 0.0
 local maxWaterHeight = Config.maxWaterHeight
 local nkhd_tsunamiSpeed = Config.tsunamiSpeed
@@ -45,17 +46,22 @@ AddEventHandler('nkhd_startTsunami_server', function()
 
     Citizen.CreateThread(function()
         while nkhd_tsunamiActive do
-            if currentWaterHeight < maxWaterHeight then
-                updateWaterHeight(currentWaterHeight + nkhd_tsunamiSpeed)
-            else
-                Citizen.Wait(nkhd_tsunamiWaitTime)
-                nkhd_tsunamiActive = false
+            if nkhd_tsunamiPaused == false then
+                if currentWaterHeight < maxWaterHeight then
+                    updateWaterHeight(currentWaterHeight + nkhd_tsunamiSpeed)
+                else
+                    Citizen.Wait(nkhd_tsunamiWaitTime)
+                    nkhd_tsunamiActive = false
+                end
             end
             Citizen.Wait(WaterWaitingTime)
         end
 
         while currentWaterHeight > 0.0 do
-            updateWaterHeight(currentWaterHeight - nkhd_tsunamiSpeed)
+            if nkhd_tsunamiPaused == false then
+                updateWaterHeight(currentWaterHeight - nkhd_tsunamiSpeed)
+                Citizen.Wait(WaterWaitingTime)
+            end
             Citizen.Wait(WaterWaitingTime)
         end
     end)
@@ -63,10 +69,20 @@ end)
 
 RegisterServerEvent('nkhd_stopTsunami_server')
 AddEventHandler('nkhd_stopTsunami_server', function()
-    print('Stopping Tsunami')
     nkhd_tsunamiActive = false
     TriggerClientEvent("nkhd_tsunami:stop", -1)
 end)
+
+RegisterServerEvent('nkhd_pauseTsunami_server')
+AddEventHandler('nkhd_pauseTsunami_server', function()
+    if nkhd_tsunamiPaused == false then
+        nkhd_tsunamiPaused = true
+    else
+        nkhd_tsunamiPaused = false
+    end
+    TriggerClientEvent("nkhd_tsunami:pause", -1)
+end)
+
 
 RegisterServerEvent('reloadWater')
 AddEventHandler('reloadWater', function()
